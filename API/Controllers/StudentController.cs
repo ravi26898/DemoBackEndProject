fdbb9eb2790ab88bc3871017786e076a39f525da
ravi26898 <ravishankar.qtr@gmail.com>
2021-11-08 16:41:58 +0530

    first commit

---
+++ API/Controllers/StudentController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using API.Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class StudentController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+        public StudentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string a = @"select Sid,Sname from dbo.Student";
+            DataTable table1 = new DataTable();
+            string sqlcon1 = _configuration.GetConnectionString("ConnString");
+            SqlDataReader read1;
+            using (SqlConnection conn1 = new SqlConnection(sqlcon1))
+            {
+                conn1.Open();
+                using (SqlCommand command1 = new SqlCommand(a, conn1))
+                {
+                    read1 = command1.ExecuteReader();
+                    table1.Load(read1);
+                    read1.Close();
+                    conn1.Close();
+                }
+            }
+            return new JsonResult(table1);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Student std)
+        {
+            string b = @"insert into dbo.Student values (@Sname)";
+            DataTable table2 = new DataTable();
+            string sqlcon2 = _configuration.GetConnectionString("ConnString");
+            SqlDataReader read2;
+            using (SqlConnection conn2 = new SqlConnection(sqlcon2))
+            {
+                conn2.Open();
+                using (SqlCommand command2 = new SqlCommand(b, conn2))
+                {
+                    command2.Parameters.AddWithValue("@Sname", std.Sname);
+                    read2 = command2.ExecuteReader();
+                    table2.Load(read2);
+                    read2.Close();
+                    conn2.Close();
+                }
+            }
+            return new JsonResult("Insertion Done");
+
+        }
+
+        [HttpPut]
+        public JsonResult Put(Student std)
+        {
+            string c = @"update dbo.Student set  Sname = @Sname where  Sid= @Sid";
+            DataTable table3 = new DataTable();
+            string sqlcon3 = _configuration.GetConnectionString("ConnString");
+            SqlDataReader read3;
+            using (SqlConnection conn3 = new SqlConnection(sqlcon3))
+            {
+                conn3.Open();
+                using (SqlCommand command3 = new SqlCommand(c, conn3))
+                {
+                    command3.Parameters.AddWithValue("@Sname", std.Sname);
+                    command3.Parameters.AddWithValue("@Sid", std.Sid);
+                    read3 = command3.ExecuteReader();
+                    table3.Load(read3);
+                    read3.Close();
+                    conn3.Close();
+                }
+            }
+            return new JsonResult("Updation Done");
+
+        }
+
+        [HttpDelete]
+        public JsonResult Delete(Student std)
+        {
+            string sqlcon4 = _configuration.GetConnectionString("ConnString");
+            SqlDataReader read4;
+
+
+            string d = @"delete from dbo.Student where  Sid = @Sid ";
+            DataTable table4 = new DataTable();
+
+
+            using (SqlConnection conn4 = new SqlConnection(sqlcon4))
+            {
+                conn4.Open();
+                using (SqlCommand command4 = new SqlCommand(d, conn4))
+                {
+                    command4.Parameters.AddWithValue("@Sid", std.Sid);
+
+                    read4 = command4.ExecuteReader();
+                    table4.Load(read4);
+                    read4.Close();
+                    conn4.Close();
+                }
+            }
+            return new JsonResult("Deletion Done");
+        }
+    }
+}
---
+++ API/Models/AzureSQLContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace API.Models
+{
+    public partial class AzureSQLContext : DbContext
+    {
+        public AzureSQLContext()
+        {
+        }
+
+        public AzureSQLContext(DbContextOptions<AzureSQLContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=sample-server-backend.database.windows.net;Initial Catalog=Azure SQL;Persist Security Info=False;User ID=Ravi;Password=megha_07;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("Student");
+
+                entity.Property(e => e.Sname)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
